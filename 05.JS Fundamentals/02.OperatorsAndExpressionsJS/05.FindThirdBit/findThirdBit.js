77ce25f231e4bc98dcfecf47dd2cffd3b77ca9ae
Nottyy <antonio_marinovvvv@abv.bg>
2014-10-27 03:19:09 +0200

    update

--- 05.JS Fundamentals/02.OperatorsAndExpressionsJS/05.FindThirdBit/findThirdBit.js
+++
@@ -1,7 +0,0 @@
-﻿function onFindBiggerButtonClick() {
-    var number = parseInt(document.getElementById('tb-first').value);
-
-    var findBit = (number >> 3) & 1;
-
-    console.log('The third bit is ' + findBit);
-}
--- 05.JS Fundamentals/02.OperatorsAndExpressionsJS/08.TrapezoidAreCalculation/trapAreaCalc.js
+++
@@ -1,9 +0,0 @@
-﻿function onFindBiggerButtonClick() {
-    var sideA = parseInt(document.getElementById('tb-first').value);
-    var sideB = parseInt(document.getElementById('tb-second').value);
-    var height = parseInt(document.getElementById('tb-third').value);
-
-    var area = (sideA + sideB) / 2 * height;
-
-    console.log('The trapezoid area is: ' + area);
-}
